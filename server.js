2057e56a43c8d8fae1d750ef5b224cc0d6bb9c54
kim0773 <kim0773@trycatch.kr>
2022-12-29 22:59:53 +0900

    node.js 기본응답생성

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+
+app.listen(8080, function(){
+    console.log('listening on 8080');
+});
+
+app.get('/pet', function(요청, 응답){
+    응답.send('pet GET 응답.');
+});
+
+app.get('/', function(요청, 응답){
+    응답.sendFile(__dirname + '/index.html');
+});
